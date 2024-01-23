dc7cea9d76cb9eadf564949e33569ca9d195181a
Shalom Yomi-Ola <yomiolashalom@gmail.com>
2024-01-23 22:45:29 +0100

    This is the first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active')
+
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', function() {
+    var navBar = document.querySelectorAll('.navbar');
+    navBar.classList.add('sticky', window.scrollY > 0);
+})
